26f8fe89bc35772a4095fc81892941c2f82ed314
Samson Royal <26835888+Samsonroyal@users.noreply.github.com>
2021-01-09 00:29:02 +0300

    Add files via upload

---
+++ Demo2/script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Snakes Uganda. All rights reserved. Designed by Vortan Systems</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='contact.html'>Contact</a></li>" +
+"</ul>"; 
